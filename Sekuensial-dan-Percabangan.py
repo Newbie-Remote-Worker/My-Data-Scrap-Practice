00e062c1fb9d8a4875082a1bb29e3f16229fff4a
Raditya <radityamajid001@gmail.com>
2025-02-24 21:07:33 +0700

    Beras dan Minyak

---
+++ Sekuensial-dan-Percabangan.py
@@ -0,0 +1,26 @@
+"""
+Ngulang Materi
+"""
+
+#1. Sekuensial
+print('Mom said, “Ndut, go to Bude shop.”')
+print('Ndut replied, “what for mom?”')
+print('Mom replied “Go to the shop to buy 1 liter of coconut oil.')
+print('Ndut went to Bude shop')
+print('Ndut makes sure he bring enough money')
+print('Ndut buy 1 liter of coconut oil')
+print('Ndut go home and give the coconut oil to mom')
+
+#2. Percabangan
+
+bottle_of_coconut_oil = 100
+bag_of_rice = 10
+
+print(f'bottle_of_coconut_oil {bottle_of_coconut_oil} Ltr')
+print(f'bag_of_rice {bag_of_rice} Kg')
+
+if bottle_of_coconut_oil > 0:
+    print('Ndut makes sure he bring enough money')
+    if bag_of_rice > 0:
+        print('Ndut bought 10 Ltr')
+        print('Ndut buy 5 Kg')
