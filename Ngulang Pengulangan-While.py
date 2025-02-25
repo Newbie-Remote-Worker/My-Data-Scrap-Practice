319e9895f8761062ff58127a2c463f227ad50852
Raditya <radityamajid001@gmail.com>
2025-02-25 14:09:35 +0700

    Read books

--- Ngulang Pengulangan-While.py
+++ Ngulang Pengulangan-While.py
@@ -1,7 +1,13 @@
 #4. Pengulangan While
 
-all_books_in_library = 100
+all_books_in_library = 10
 print('librarian said, "read all books"')
 
 books_have_been_read = 0
 print(f'have been read {books_have_been_read} books')
+
+while books_have_been_read < all_books_in_library:
+    books_have_been_read = books_have_been_read + 1
+    print(f'have been read {books_have_been_read} books')
+
+print(f'all books {books_have_been_read} have been read')
